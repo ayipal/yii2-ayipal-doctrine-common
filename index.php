8161740f555cbfbcfac1bf89f973a37821cbedf8
Andreas Prucha (Abexto/Helicon) <andreas.prucha@gmail.com>
2017-09-07 05:31:23 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Copyright 2017 Andreas Prucha, Abexto - Helicon Software Development.
+-->
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    // put your code here
+    ?>
+  </body>
+</html>
